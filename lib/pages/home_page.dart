3fbdbdf492f09e355894c91ca512ac374e435779
Asmit Gupta <asmitwrites@gmail.com>
2022-10-13 17:32:37 +0530

    Added more products

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,28 +1,52 @@
 // ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catalog/models/catalog.dart';
 // import 'package:flutter_catalog/main.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 1;
 
   @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    final productsData = decodeData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummylist = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
       body: ListView.builder(
-        itemCount: dummylist.length,
+        itemCount: CatalogModel.items.length,
         itemBuilder: (context, index) {
           return ItemWidget(
-            item: dummylist[index],
+            item: CatalogModel.items[index],
           );
         },
       ),
